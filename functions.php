3145c20320d5ae05789ebdc4ce3cfebbad1e675a
Luca Fregoso <luca.fregoso@gmail.com>
2022-07-29 18:52:18 +0200

    Require inc files using locate_template
    To allow child themes to override inc methods

--- functions.php
+++ functions.php
@@ -24,34 +24,34 @@ $storefront = (object) array(
 	/**
 	 * Initialize all the things.
 	 */
-	'main'       => require 'inc/class-storefront.php',
-	'customizer' => require 'inc/customizer/class-storefront-customizer.php',
+	'main'       => require locate_template('inc/class-storefront.php'),
+	'customizer' => require locate_template('inc/customizer/class-storefront-customizer.php'),
 );
 
-require 'inc/storefront-functions.php';
-require 'inc/storefront-template-hooks.php';
-require 'inc/storefront-template-functions.php';
-require 'inc/wordpress-shims.php';
+require locate_template('inc/storefront-functions.php');
+require locate_template('inc/storefront-template-hooks.php');
+require locate_template('inc/storefront-template-functions.php');
+require locate_template('inc/wordpress-shims.php');
 
 if ( class_exists( 'Jetpack' ) ) {
-	$storefront->jetpack = require 'inc/jetpack/class-storefront-jetpack.php';
+	$storefront->jetpack = require locate_template('inc/jetpack/class-storefront-jetpack.php');
 }
 
 if ( storefront_is_woocommerce_activated() ) {
-	$storefront->woocommerce            = require 'inc/woocommerce/class-storefront-woocommerce.php';
-	$storefront->woocommerce_customizer = require 'inc/woocommerce/class-storefront-woocommerce-customizer.php';
+	$storefront->woocommerce            = require locate_template('inc/woocommerce/class-storefront-woocommerce.php');
+	$storefront->woocommerce_customizer = require locate_template('inc/woocommerce/class-storefront-woocommerce-customizer.php');
 
-	require 'inc/woocommerce/class-storefront-woocommerce-adjacent-products.php';
+	require locate_template('inc/woocommerce/class-storefront-woocommerce-adjacent-products.php');
 
-	require 'inc/woocommerce/storefront-woocommerce-template-hooks.php';
-	require 'inc/woocommerce/storefront-woocommerce-template-functions.php';
-	require 'inc/woocommerce/storefront-woocommerce-functions.php';
+	require locate_template('inc/woocommerce/storefront-woocommerce-template-hooks.php');
+	require locate_template('inc/woocommerce/storefront-woocommerce-template-functions.php');
+	require locate_template('inc/woocommerce/storefront-woocommerce-functions.php');
 }
 
 if ( is_admin() ) {
-	$storefront->admin = require 'inc/admin/class-storefront-admin.php';
+	$storefront->admin = require locate_template('inc/admin/class-storefront-admin.php');
 
-	require 'inc/admin/class-storefront-plugin-install.php';
+	require locate_template('inc/admin/class-storefront-plugin-install.php');
 }
 
 /**
@@ -60,9 +60,9 @@ if ( is_admin() ) {
  * https://core.trac.wordpress.org/ticket/39610?cversion=1&cnum_hist=2
  */
 if ( version_compare( get_bloginfo( 'version' ), '4.7.3', '>=' ) && ( is_admin() || is_customize_preview() ) ) {
-	require 'inc/nux/class-storefront-nux-admin.php';
-	require 'inc/nux/class-storefront-nux-guided-tour.php';
-	require 'inc/nux/class-storefront-nux-starter-content.php';
+	require locate_template('inc/nux/class-storefront-nux-admin.php');
+	require locate_template('inc/nux/class-storefront-nux-guided-tour.php');
+	require locate_template('inc/nux/class-storefront-nux-starter-content.php');
 }
 
 /**
